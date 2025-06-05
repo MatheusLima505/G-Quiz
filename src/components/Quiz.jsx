6fcd33fe3748dff1ea75b539b4965fc63f5aeccd
Matheus Araujo de Lima <20221074010010@ifrn.local>
2025-06-05 16:24:41 -0300

    quiz update

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,6 +4,16 @@ import perguntas from "./../assets/questions.json";
 function Quiz() {
   const [thisQuestion, setThisQuestion] = useState(null);
 
+  function handleResposta(alt) {
+    if (alt == true) {
+      const QuestionIndex = Math.floor(Math.random() * perguntas.length);
+      setThisQuestion(perguntas[QuestionIndex]);
+    }
+    if (alt == false) {
+        alert("foase")
+    }
+  }
+
   useEffect(() => {
     const QuestionIndex = Math.floor(Math.random() * perguntas.length);
     setThisQuestion(perguntas[QuestionIndex]);
@@ -14,12 +24,34 @@ function Quiz() {
   return (
     <>
       <h1>quiz time</h1>
-      <div className="question"><h2>{thisQuestion.pergunta}</h2></div>
+      <div className="question">
+        <h2>{thisQuestion.pergunta}</h2>
+      </div>
       <div className="answers">
-            <button className="item">{thisQuestion.alternativas[0].texto}</button>
-            <button className="item">{thisQuestion.alternativas[1].texto}</button>
-            <button className="item">{thisQuestion.alternativas[2].texto}</button>
-            <button className="item">{thisQuestion.alternativas[3].texto}</button>
+        <button
+          className="item"
+          onClick={() =>handleResposta(thisQuestion.alternativas[0].correta)}
+        >
+          {thisQuestion.alternativas[0].texto}
+        </button>
+        <button
+          className="item"
+          onClick={() =>handleResposta(thisQuestion.alternativas[1].correta)}
+        >
+          {thisQuestion.alternativas[1].texto}
+        </button>
+        <button
+          className="item"
+          onClick={() =>handleResposta(thisQuestion.alternativas[2].correta)}
+        >
+          {thisQuestion.alternativas[2].texto}
+        </button>
+        <button
+          className="item"
+          onClick={() =>handleResposta(thisQuestion.alternativas[3].correta)}
+        >
+          {thisQuestion.alternativas[3].texto}
+        </button>
       </div>
     </>
   );
