89182841fdd8a01ddbe05dea59c2a7f9f40e2e6d
MatheusLima505 <mathlegeferi@gmail.com>
2025-06-25 14:24:02 -0300

    credits added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ function Start({ handleStart}){
     return(
         <>
         <h1>G-Quiz</h1>
+        <p>por Matheus Araujo de Lima</p>
         <button onClick={handleStart}>Start</button>
         </>
     )
